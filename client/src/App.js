913fd1996a8fec8db64eae392c0990674b59e84e
Scott Grobe <gitgrobe@gmail.com>
2019-11-10 23:41:24 -0500

    remove material-UI and add regualr styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,42 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
-import { NavBar } from './components/NavBar';
-import { PlayerCard } from './components/PlayerCard';
+import { NavBar } from "./components/NavBar";
+import { PlayerCard } from "./components/PlayerCard";
 
-
-import './index.scss'
+import "./index.scss";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      players: [],
-    }
+      players: []
+    };
   }
 
   componentDidMount() {
     axios
       .get(`http://localhost:5000/api/players`)
       .then(res => {
-        this.setState({ players: res.data})})
-      .catch(err => console.log(err, 'ERROR cannot fetch data!'))
-  };
+        this.setState({ players: res.data });
+      })
+      .catch(err => console.log(err, "ERROR cannot fetch data!"));
+  }
 
-  render () {
-    
+  render() {
     return (
       <div>
-      <NavBar />
-        {this.state.players.map(player => (
-          <PlayerCard key={player.id} name={player.name} searches={player.searches}  />
-        ))}
+        <NavBar />
+        <div className='container'>
+          {this.state.players.map(player => (
+            <PlayerCard
+              id={player.id + 1}
+              key={player.id}
+              name={player.name}
+              searches={player.searches}
+            />
+          ))}
+        </div>
       </div>
     );
   }
